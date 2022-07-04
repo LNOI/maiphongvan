b904fb57cec78af21af050df52b28dd7cc5c70c3
LNOI <19521771@gm.uit.edu.vn>
2022-07-05 06:57:34 +0700

    udae

--- rest/tests.py
+++ rest/tests.py
@@ -1,4 +1,13 @@
 from django.test import TestCase
-
+from django.db import connection
 # Create your tests here.
-print("Test Successfully")
+
+def Test():
+    with connection.cursor() as cursor:
+        cursor.execute("create tables account ( username varchar(100) )")
+        
+        cursor.execute("insert into account values ('nguyenthanh')")
+        cursor.execute("select * from account")
+        row=cursor.fetchall()
+        print(row)
+Test()
